00c9fb96095164bb250d324fae05a18fca6f0627
Amir Dhahri <amirdhahri01@gmail.com>
2024-09-28 21:58:12 +0100

    The first section is done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const icons = document.querySelectorAll(".section-1-icons i");
+let counter = 1;
 setInterval(() => {
-    const icon =  document.querySelector(".section-1-icons i.change");
+    const icon = document.querySelector(".section-1-icons i.change");
     icon.classList.remove("change");
-    icon.nextElementSibling.classList.add("change");
-}, 1000);
+    counter++;
+    if (counter > icons.length) {
+        counter = 1;
+        icons[0].classList.add("change");
+    } else {
+        icon.nextElementSibling.classList.add("change");
+    }
+}, 4000);
