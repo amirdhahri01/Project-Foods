9cdd878a568cda8c0eddf9d04c607a8e99be027c
Amir Dhahri <amirdhahri01@gmail.com>
2024-09-29 17:38:17 +0100

    The navbar,menu, and thier animations is done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const menu = document.querySelector(".menu");
 const icons = document.querySelectorAll(".section-1-icons i");
 let counter = 1;
 setInterval(() => {
@@ -11,3 +12,8 @@ setInterval(() => {
         icon.nextElementSibling.classList.add("change");
     }
 }, 4000);
+menu.addEventListener("click", () => {
+    document.querySelectorAll(".target").forEach(item => {
+        item.classList.toggle("change");
+    });
+})
